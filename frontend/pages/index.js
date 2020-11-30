07959c36823003c145d7a314da86cde3916ae02b
ViniGarcia10 <viniciusgarcia49@gmail.com>
2020-11-30 15:07:39 -0400

    Update Production

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -31,29 +31,11 @@ export default function Home() {
       </Head>
 
       <Header />
-
-<<<<<<< HEAD
       <Main>
         <Welcome />
         <About />
         <Projects />
       </Main>
-=======
-        <section className={styles.About}>
-          <img
-            className={styles.avatar}
-            src="./avatar.png"
-            alt="Vinícius Garcia Leão"
-          />
-          <p className={styles.descriptionAbout}>
-            Sou Vinícius Garcia, desenvolvedor de softwares
-            <br /> com alta performance, sempre buscando aprender
-            <br /> novas tecnologias para agregar aos meus trabalhos.
-          </p>
-        </section>
-      </main>
->>>>>>> f818ba62074b13bae90d2faa5cd1c26540b1ca7b
-
       <Footer />
     </Container>
   );
